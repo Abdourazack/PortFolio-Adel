4c1a7b6118f2f2b694dcf5d62c654aaba2fb0f1b
Adel <adelhared123@gmail.com>
2025-11-21 07:52:11 +0100

    Ajout de nouveaux fichiers / mise à jour

--- src/app/components/education/education.ts
+++ src/app/components/education/education.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-education',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './education.html',
   styleUrl: './education.scss',
 })
 export class Education {
 
+    formations = [
+    {
+      title: 'Bachelier en Informatique',
+      status: 'En dernière année',
+      details: [
+        'Projets full stack : Angular, Node.js, Express, NestJS',
+        'Développement front-end et back-end',
+        'Bases en Python et scripting Kali Linux',
+        'Gestion de bases de données : MySQL, MongoDB',
+        'Utilisation de Docker, VirtualBox, VMware',
+        'Notions de réseaux et administration système',
+        'Méthodologie Agile pour la gestion de projets'
+      ]
+    }
+  ];
 }
