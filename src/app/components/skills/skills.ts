d1f238b6b1f85f7233646759a290327929977b3b
Adel <adelhared123@gmail.com>
2025-11-21 04:41:54 +0100

    Mise à jour des dernières modifications

--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -10,14 +10,26 @@ import { Component } from '@angular/core';
 export class Skills {
 
     skills = [
-    { name: 'HTML', level: 95 },
-    { name: 'CSS / SCSS', level: 90 },
-    { name: 'JavaScript', level: 85 },
-    { name: 'Angular', level: 80 },
-    { name: 'Node.js', level: 80 },
-    { name: 'NestJS', level: 75 },
-    { name: 'Express', level: 70 },
-    { name: 'Git', level: 85 }
+  { name: 'HTML', level: 95 },
+  { name: 'CSS / SCSS', level: 90 },
+  { name: 'JavaScript', level: 85 },
+  { name: 'TypeScript', level: 80 },
+  { name: 'Angular', level: 80 },
+  { name: 'Node.js', level: 80 },
+  { name: 'NestJS', level: 75 },
+  { name: 'Express', level: 70 },
+  { name: 'Python', level: 65 },
+  { name: 'Git', level: 85 },
+  { name: 'MongoDB', level: 70 },
+  { name: 'Mongo Compass', level: 65 },
+  { name: 'MySQL / PHPMyAdmin', level: 70 },
+  { name: 'Docker', level: 60 },
+  { name: 'VirtualBox / VMware', level: 60 },
+  { name: 'Linux (bases)', level: 60 },
+  { name: 'Windows', level: 80 },
+  { name: 'Réseau (bases)', level: 60 },
+  { name: 'Gestion de projet / Agile', level: 65 },
+  { name: 'Scripting Kali Linux', level: 60 }
   ];
 
 }
