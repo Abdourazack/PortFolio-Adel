bf1e59913ef05363d426e96062636f08aeae97d6
Adel <adelhared123@gmail.com>
2025-11-21 10:05:15 +0100

    Ajout de nouveaux fichiers / mise à jour

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -34,14 +34,26 @@ export class Projects {
       code: 'https://github.com/Abdourazack/gestion-article-mvc.git',
       link: 'https://gestionarticle.alwaysdata.net/'
     },
-  {
-  title: 'Site Vitrine - Bistrot de Quartier',
-  description: `Site vitrine simple pour un bistrot de quartier, réalisé dans le cadre de la première année en HTML et CSS. 
-  Le site présente l'accueil, le menu, les horaires et les contacts du bistrot, avec une mise en page responsive et un design convivial.`,
-  technologies: ['HTML', 'CSS', 'Responsive Design, Bootstrap'],
-  code: 'https://github.com/Abdourazack/L-Endroit.git',
-  link: 'https://lapasserelle.netlify.app'
+    {
+      title: 'Site Vitrine - Bistrot de Quartier',
+      description: `Site vitrine simple pour un bistrot de quartier, réalisé dans le cadre de la première année en HTML et CSS. 
+      Le site présente l'accueil, le menu, les horaires et les contacts du bistrot, avec une mise en page responsive et un design convivial.`,
+      technologies: ['HTML', 'CSS', 'Responsive Design, Bootstrap'],
+      code: 'https://github.com/Abdourazack/L-Endroit.git',
+      link: 'https://lapasserelle.netlify.app'
+    },
+
+    {
+  title: 'API REST - Gestion Librairie',
+  description: `API REST complète pour la gestion d'une librairie, construite avec NestJS. 
+  Cette API permet de gérer les livres, auteurs et utilisateurs avec des opérations CRUD, 
+  et expose des endpoints sécurisés et structurés pour l'accès aux données. 
+  La base de données MongoDB (NoSQL) est organisée en collections pour une gestion flexible des données. 
+  Docker est utilisé pour le déploiement et l'isolation de l'environnement, et MongoDB Compass sert d'interface graphique pour la visualisation et la gestion de la base de données.`,
+  technologies: ['NestJS', 'Node.js', 'TypeScript', 'MongoDB', 'Docker', 'MongoDB Compass', 'REST API', 'NoSQL', 'CRUD'],
+  code: 'https://github.com/Abdourazack/Librairie.git'
 }
+
 
   ];
 }
